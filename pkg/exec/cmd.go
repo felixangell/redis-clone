08f1c780c8c2cbe8e5d2bcb45ca63a2ecc41f351
Felix Angell <hello@felix.engineer>
2023-02-08 23:51:18 +0000

    fix: slight refactor to HELLO command to take config map from orchestrator

--- pkg/exec/cmd.go
+++ pkg/exec/cmd.go
@@ -11,30 +11,10 @@ type Command interface {
 
 type HelloCommand struct{}
 
-func (h HelloCommand) Execute(_ *KafkaNodeOrchestrator, args ...api.Value) api.Value {
+func (h HelloCommand) Execute(o *KafkaNodeOrchestrator, args ...api.Value) api.Value {
 	// HELLO command returns a MAP of the configuration for this
 	// cluster according to the redis spec.
-
-	// TODO grab this from orchestrator
-	// research all the values to send incl mandatory
-	// and whats available.
-	configMap := api.KeyValueMap{
-		"REDIS000000000017.0": "",
-		"run_id":              "54a7ed1c17b0a7f9dbb1286b4e64a14f88c0dde7",
-		"cluster_enabled":     "1",
-		"cluster_state":       "ok",
-		"cluster_slots_pfail": "0",
-		"cluster_slots_fail":  "0",
-		"cluster_known_nodes": "1",
-		"cluster_size":        "1",
-		//"cluster_slots_ok":                "16384",
-		//"cluster_slots_assigned":          "16384",
-		//"cluster_current_epoch":           "7",
-		//"cluster_my_epoch":                "6",
-		//"cluster_stats_messages_sent":     "2174",
-		//"cluster_stats_messages_received": "2173",
-	}
-	return configMap
+	return o.ClusterState()
 }
 
 type SetCommand struct{}
--- pkg/exec/orchestrator.go
+++ pkg/exec/orchestrator.go
@@ -1,6 +1,8 @@
 package exec
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"github.com/bat-labs/krake/cache"
 	"github.com/bat-labs/krake/pkg/api"
 	"log"
@@ -8,11 +10,26 @@ import (
 )
 
 type KafkaNodeOrchestrator struct {
+	id      string
 	backend cache.Cache
 }
 
+func generateRunID() (string, error) {
+	b := make([]byte, 20)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(b), nil
+}
+
 func NewKafkaNodeOrchestrator(backend cache.Cache) *KafkaNodeOrchestrator {
-	return &KafkaNodeOrchestrator{backend: backend}
+	id, err := generateRunID()
+	if err != nil {
+		panic(err)
+	}
+
+	return &KafkaNodeOrchestrator{id: id, backend: backend}
 }
 
 func (o *KafkaNodeOrchestrator) Submit(conn net.Conn, response api.Value) {
@@ -25,4 +42,26 @@ func (o *KafkaNodeOrchestrator) Submit(conn net.Conn, response api.Value) {
 
 func (o *KafkaNodeOrchestrator) CacheBackend() cache.Cache {
 	return o.backend
+}
+
+func (o *KafkaNodeOrchestrator) ClusterState() api.Value {
+	return api.KeyValueMap{
+		//"REDIS000000000017.0": "",
+		"run_id": o.id,
+
+		"redis_mode": "standalone",
+
+		"cluster_enabled":     "1",
+		"cluster_state":       "ok",
+		"cluster_slots_pfail": "0",
+		"cluster_slots_fail":  "0",
+		"cluster_known_nodes": "1",
+		"cluster_size":        "1",
+		//"cluster_slots_ok":                "16384",
+		//"cluster_slots_assigned":          "16384",
+		//"cluster_current_epoch":           "7",
+		//"cluster_my_epoch":                "6",
+		//"cluster_stats_messages_sent":     "2174",
+		//"cluster_stats_messages_received": "2173",
+	}
 }

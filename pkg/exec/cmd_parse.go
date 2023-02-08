b9cece0b105dde2b57d988d4956f6ee48a1574d9
Felix Angell <hello@felix.engineer>
2023-02-08 22:44:33 +0000

    fix: bind on no explicit host
    before we bound to localhost which meant we could not expose traffic outside of docker

--- pkg/exec/cmd_parse.go
+++ pkg/exec/cmd_parse.go
@@ -21,14 +21,46 @@ func ParseCommand(v api.Value) (Command, error) {
 	cmd := strings.ToLower(string(cmdNameMsg.Data[:cmdNameMsg.Length]))
 
 	switch cmd {
+
+	// internal
+
 	case "hello":
 		return NewHelloCommand(bs.Data[1:]), nil
+
+	// key based operations
 
 	case "set":
 		return NewSetCommand(bs.Data[1:]), nil
 
 	case "get":
 		return NewGetCommand(bs.Data[1:]), nil
+
+	case "del":
+		panic("not yet implemented")
+
+	// unordered hash set operations
+
+	case "hset":
+		// hset hash-key k1 v1
+		panic("not yet implemented")
+
+	case "hget":
+		// hget hash-key k1
+		panic("not yet implemented")
+
+	case "hgetall":
+		// entire hash content
+		panic("not yet implemented")
+
+	case "hdel":
+		// hdel hash-key k1
+		panic("not yet implemented")
+
+		// sets
+
+		// bitmap
+
+		// zsets
 	}
 
 	panic(fmt.Sprintf("Unhandled command %s", cmd))
